c462e69de257ab44f79dad5656fef14c1aa4461e
OlgaKryushycheva <115803033+OlgaKryushycheva@users.noreply.github.com>
2023-07-07 13:17:29 +0300

    алгоритм

--- index.js
+++ index.js
@@ -80,3 +80,103 @@ invokeAction(options);
 // });
 
 // invokeAction({ action: "remove", id: "mbp_OS45vZcARAcL7VDvs" });
+
+// =========================================================
+// алгоритм:
+/*
+1. файл - контактс.js 
+подключить fs = require("fs/promice")
+все функции обработчки операцый
+- асинхронніе
+- fs.list...
+- fs.write...
+- fs/append...
+
+path = require("path")
+
+contactPath = path.join(__dirname, "db", "contacts.json");
+
+это путь к файлу json - єто ужно чтобы записывать изменения в єтот файл
+fs.list...(contactPath, ()=>{} /JSON.parse или stringify)
+
+читать:
+ ()=>{
+ буфер = fs.readFile(contactsPath);
+contacts - JSON.parse(buffer);
+  return contacts;
+}
+
+читать 1
+ (id)=>{
+    получить все 
+     Contact = вызов list()
+
+     найти через файндИнд
+     Contact1 = Contact.файнд
+})
+
+
+добавить
+(data все кроме id)=>{
+     получить все 
+     Contact = вызов list()
+
+newContact = {
+    id = nanoid()
+    data
+}
+
+запушить новфй контакт contacts.push(newContact);
+записать все в json 
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+})
+
+обновить
+(все-все даные) {
+    получить все 
+     Contact = вызов list()
+     
+     найти по id файндИндекс
+if (index === -1) {
+    return null;
+  }
+
+  обновить
+  Contacts[i] = {
+  все данные новые и старые
+  }
+
+  записать все в json fs.write...
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+удалить 
+(id)=>{
+    получить все 
+     Contact = вызов list()
+
+найти по id файндИндекс
+if (index === -1) {
+    return null;
+  }
+    через слайс удалить по id
+    [ ремувContact] = Contact.слайс(і, 1)
+    записать все в json fs.write...
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+})
+
+
+index.js
+
+1 фукция, которая читает єкшені через свитч: 
+єкшен - візов соответсвующей функции
+ретерн консоль лог
+все асинхронно
+
+візов функций - 
+параметі - деструктуризвано єкшен обязательно, остальное если есть
+
+настроить коммандер
+візов функции(параметр - 1 из командора)
+в консоли - нод индекс екшн необходимые_параметри
+*/
